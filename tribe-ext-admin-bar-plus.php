12e81696b49efe1a22cc2253b16eb0471ed6b1e6
András Guseo <andras@divi-magazine.com>
2020-02-26 23:30:35 +0100

    Removed unneeded code

--- tribe-ext-admin-bar-plus.php
+++ tribe-ext-admin-bar-plus.php
@@ -58,18 +58,6 @@ if (
 		private $class_loader;
 
 		/**
-		 * @var Settings
-		 */
-		private $settings;
-
-		/**
-		 * Custom options prefix (without trailing underscore).
-		 *
-		 * Should leave blank unless you want to set it to something custom, such as if migrated from old extension.
-		 */
-		private $opts_prefix = '';
-
-		/**
 		 * Is Events Calendar PRO active. If yes, we will add some extra functionality.
 		 *
 		 * @return bool
@@ -84,40 +72,12 @@ if (
 		public $et_active = false;
 
 		/**
-		 * Is Filter Bar active. If yes, we will add some extra functionality.
-		 *
-		 * @return bool
-		 */
-		public $fb_active = false;
-
-		/**
 		 * Setup the Extension's properties.
 		 *
 		 * This always executes even if the required plugins are not present.
 		 */
 		public function construct() {
-			// Dependency requirements and class properties can be defined here.
-
-			/**
-			 * Examples:
-			 * All these version numbers are the ones on or after November 16, 2016, but you could remove the version
-			 * number, as it's an optional parameter. Know that your extension code will not run at all (we won't even
-			 * get this far) if you are not running The Events Calendar 4.3.3+ or Event Tickets 4.3.3+, as that is where
-			 * the Tribe__Extension class exists, which is what we are extending.
-			 *
-			 * If using `tribe()`, such as with `Tribe__Dependency`, require TEC/ET version 4.4+ (January 9, 2017).
-			 */
-			// $this->add_required_plugin( 'Tribe__Tickets__Main', '4.4' );
-			// $this->add_required_plugin( 'Tribe__Tickets_Plus__Main', '4.3.3' );
-			// $this->add_required_plugin( 'Tribe__Events__Main', '4.4' );
-			// $this->add_required_plugin( 'Tribe__Events__Pro__Main', '4.3.3' );
-			// $this->add_required_plugin( 'Tribe__Events__Community__Main', '4.3.2' );
-			// $this->add_required_plugin( 'Tribe__Events__Community__Tickets__Main', '4.3.2' );
-			// $this->add_required_plugin( 'Tribe__Events__Filterbar__View', '4.3.3' );
-			// $this->add_required_plugin( 'Tribe__Events__Tickets__Eventbrite__Main', '4.3.2' );
-			// $this->add_required_plugin( 'Tribe_APM', '4.4' );
-
-			// Conditionally-require Events Calendar PRO. If it is active, run an extra bit of code.
+			// Conditionally-require Events Calendar PRO or Event Tickets. If it is active, run an extra bit of code.
 			add_action( 'tribe_plugins_loaded', [ $this, 'detect_tribe_plugins' ], 0 );
 		}
 
@@ -139,23 +99,6 @@ if (
 				$this->add_required_plugin( 'Tribe__Tickets__Main' );
 				$this->et_active = true;
 			}
-			if ( $dep->is_plugin_active( 'Tribe__Events__Filterbar__View' ) ) {
-				$this->add_required_plugin( 'Tribe__Events__Filterbar__View' );
-				$this->fb_active = true;
-			}
-		}
-
-		/**
-		 * Get Settings instance.
-		 *
-		 * @return Settings
-		 */
-		private function get_settings() {
-			if ( empty( $this->settings ) ) {
-				$this->settings = new Settings( $this->opts_prefix );
-			}
-
-			return $this->settings;
 		}
 
 		/**
@@ -172,14 +115,7 @@ if (
 
 			$this->class_loader();
 
-			$this->get_settings();
-
-			// TODO: Just a test. Remove this.
-			//$this->testing_hello_world();
-
 			// Insert filter and action hooks here
-			//add_filter( 'thing_we_are_filtering', [ $this, 'my_custom_function' ] );
-
 			add_action('admin_bar_menu', [ $this, 'add_toolbar_items' ], 100);
 		}
 
@@ -207,70 +143,6 @@ if (
 			}
 
 			return true;
-		}
-
-		/**
-		 * Use Tribe Autoloader for all class files within this namespace in the 'src' directory.
-		 *
-		 * TODO: Delete this method and its usage throughout this file if there is no `src` directory, such as if there are no settings being added to the admin UI.
-		 *
-		 * @return Tribe__Autoloader
-		 */
-		public function class_loader() {
-			if ( empty( $this->class_loader ) ) {
-				$this->class_loader = new Tribe__Autoloader;
-				$this->class_loader->set_dir_separator( '\\' );
-				$this->class_loader->register_prefix(
-					NS,
-					__DIR__ . DIRECTORY_SEPARATOR . 'src'
-				);
-			}
-
-			$this->class_loader->register_autoloader();
-
-			return $this->class_loader;
-		}
-
-		/**
-		 * TODO: Testing Hello World. Delete this for your new extension.
-		 */
-		public function testing_hello_world() {
-			$message = sprintf( '<p>Hello World from %s. Make sure to remove this in your own new extension.</p>', '<strong>' . $this->get_name() . '</strong>' );
-
-			$message .= sprintf( '<p><strong>Bonus!</strong> Get one of our own custom option values: %s</p><p><em>See the code to learn more.</em></p>', $this->get_one_custom_option() );
-
-			tribe_notice( PLUGIN_TEXT_DOMAIN . '-hello-world', $message, [ 'type' => 'info' ] );
-		}
-
-		/**
-		 * Demonstration of getting this extension's `a_setting` option value.
-		 *
-		 * TODO: Rework or remove this.
-		 *
-		 * @return mixed
-		 */
-		public function get_one_custom_option() {
-			$settings = $this->get_settings();
-
-			return $settings->get_option( 'a_setting', 'https://theeventscalendar.com/' );
-		}
-
-		/**
-		 * Get all of this extension's options.
-		 *
-		 * @return array
-		 */
-		public function get_all_options() {
-			$settings = $this->get_settings();
-
-			return $settings->get_all_options();
-		}
-
-		/**
-		 * Include a docblock for every class method and property.
-		 */
-		public function my_custom_function() {
-			// do your custom stuff
 		}
 
 		function add_toolbar_items( $admin_bar ) {
@@ -308,20 +180,6 @@ if (
 					),
 				));
 			}
-
-//			// Inject Filter Bar settings
-//			if ( $this->fb_active ) {
-//				$admin_bar->add_menu( array(
-//					'id'    => 'tribe-events-settings-filters',
-//					'parent' => 'tribe-events-settings',
-//					'title' => 'Filters (FB)',
-//					'href'  => 'edit.php?page=tribe-common&tab=event-tickets&post_type=tribe_events',
-//					'meta'  => array(
-//						'title' => __('Filters (FB)'),
-//						'class' => 'my_menu_item_class'
-//					),
-//				));
-//			}
 
 			// Inject Events Calendar PRO settings
 			if ( $this->ecp_active ) {
@@ -377,16 +235,6 @@ if (
 					'class' => 'my_menu_item_class'
 				),
 			));
-/*			$admin_bar->add_menu( array(
-				'id'    => 'tribe-events-settings-upgrade',
-				'parent' => 'tribe-events-settings-display',
-				'title' => 'Display',
-				'href'  => 'edit.php?page=tribe-common&tab=display&post_type=tribe_events',
-				'meta'  => array(
-					'title' => __('Display'),
-					'class' => 'my_menu_item_class'
-				),
-			));*/
 		}
 
 	} // end class
